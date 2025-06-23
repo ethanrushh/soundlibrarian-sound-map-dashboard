385cc1beb52a6ebd210a2d2e46adcfd1fb2269ba
Ethan Rushbrook <ethanrushbrook@outlook.com>
2025-06-23 18:40:42 +1000

    feat: prod api url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,4 +9,4 @@ export function isDevelopment() {
     return !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 }
 
-export const apiUrl = () => isDevelopment() ? 'http://localhost:5077/api/v1' : ''
+export const apiUrl = () => isDevelopment() ? 'http://localhost:5077/api/v1' : 'https://server.ehvbfwjsdhuifsayaas.com/api/v1'
